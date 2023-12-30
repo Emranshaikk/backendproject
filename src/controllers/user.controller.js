78e3781d9cb89b24fde47a33be5d1a2920dfc3e6
Emranshaikk <imran22shaik2022@outlook.com>
2023-12-30 13:19:39 +0530

    fix all registered controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,8 +17,8 @@ const registerUser = asyncHandler(async (req, res) => {
    // return response 
 
     const { fullName, email, username, password} = req.body
-    console.log("email:", email);
-    console.log("fullName:", fullName);
+    //console.log("email:", email);
+    //console.log("fullName:", fullName);
 
     if (
         [ fullName, email, username, password].some((field) => field?.trim()=== "")
@@ -34,10 +34,18 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "User with email and Username already Exist")
     }
 
+    console.log(req.files);
+
     //console.log(req.file)
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+            coverImageLocalPath = req.files.coverImage[0].path
+        
+    };
 
     if (!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
